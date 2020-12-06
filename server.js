5fefa451dbcdf6a407ca570090a5cb80d31bff05
anongtf <anongtf26@gmail.com>
2020-12-06 22:10:50 +0700

    change root response

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  // connect to your own database here
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    // connect to your own database here
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'aneagoie',
+        password: '',
+        database: 'smart-brain'
+    }
 });
 
 const app = express();
@@ -25,13 +25,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res) => { res.send("Hello world") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
 })
